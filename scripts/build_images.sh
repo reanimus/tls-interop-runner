2bbe6822ba275cc2ca034bf516c8a172449763aa
Mingtao Yang <mt@empty.fun>
2021-09-13 16:58:32 -0700

    Build network

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,6 +1,11 @@
-#!/bin/bash -uex
+#!/bin/bash
 #
-# Builds and tags the interop images
+# Builds and tags the interop images that will be used for the docker-compose.yml
+# environment
+
+THIS_SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+set -uexo pipefail
 
 do_build() {
     local dir
@@ -13,8 +18,8 @@ do_build() {
         image="$("$dir/build")"
     else
         [ ! -f "$dir/Dockerfile" ] && { echo "Directory $dir missing Dockerfile." >&2; exit 1; }
-        imgfile="$(mktempi build.XXXX)"
-        (cd "$dir" && docker build --iidfile "$imgfile" >&2 )
+        imgfile="$(mktemp)"
+        (cd "$dir" && docker build --iidfile "$imgfile" . >&2 )
         image="$(cat "$imgfile")"
     fi
     echo "$image"
@@ -22,3 +27,4 @@ do_build() {
 
 docker image tag "$(do_build "$CLIENT_SRC/$CLIENT")" "tls-endpoint-$CLIENT"
 docker image tag "$(do_build "$SERVER_SRC/$SERVER")" "tls-endpoint-$SERVER"
+docker image tag "$(do_build "$THIS_SCRIPT_DIR/../network")" "tls-interop-network"
